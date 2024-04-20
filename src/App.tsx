7fb7e2cb789cac33aeeb5f5b5cb5f370f05ba46b
Taavi S <21169621+taasko@users.noreply.github.com>
2024-04-20 12:46:42 +0300

    build(deploy): base url for router also

--- src/App.tsx
+++ src/App.tsx
@@ -8,43 +8,46 @@ import ICal from "./Events/ICal";
 
 import events from "./events.json";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage />,
 
-    children: [
-      {
-        path: "events",
-        loader: () => ({ events }),
-        children: [
-          {
-            path: "list",
-            loader: () => ({ events }),
-            element: <EventList />,
-          },
-          {
-            path: ":eventId",
-            loader: ({ params }) => {
-              const { eventId } = params;
-
-              const event = events.find((e) => e.id === Number(eventId));
-
-              return { event };
+      children: [
+        {
+          path: "events",
+          loader: () => ({ events }),
+          children: [
+            {
+              path: "list",
+              loader: () => ({ events }),
+              element: <EventList />,
             },
-            element: <EventView />,
-          },
-          {
-            path: "ical",
-            loader: () => ({ events }),
-            element: <ICal />,
-          },
-        ],
-      },
-    ],
-  },
-]);
+            {
+              path: ":eventId",
+              loader: ({ params }) => {
+                const { eventId } = params;
+
+                const event = events.find((e) => e.id === Number(eventId));
+
+                return { event };
+              },
+              element: <EventView />,
+            },
+            {
+              path: "ical",
+              loader: () => ({ events }),
+              element: <ICal />,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
 
 export default function App() {
   return <RouterProvider router={router} />;
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => ({
   plugins: [react()],
 
-  base: command === "build" ? "jamit-app" : "/",
+  base: command === "build" ? "/jamit-app" : "/",
 
   css: {
     modules: {
