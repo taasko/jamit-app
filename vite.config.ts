3799062425126d127e57b0ab9246f49748c99d3a
Taavi S <21169621+taasko@users.noreply.github.com>
2024-04-20 12:25:30 +0300

    build(deploy): set base url to another when building

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+
+  base: command === "build" ? "jamit-app" : "/",
 
   css: {
     modules: {
       localsConvention: "camelCase",
     },
   },
-});
+}));
