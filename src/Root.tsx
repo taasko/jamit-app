bb516dc0184d8fbf15275b32c694263c29fe5be6
Taavi S <21169621+taasko@users.noreply.github.com>
2024-04-21 13:54:55 +0300

    refactor: navbar to own component with css module

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,19 +3,7 @@ import { Link, Outlet } from "react-router-dom";
 export default function Root() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to={"/"}>/</Link>
-          </li>
-          <li>
-            <Link to={"/events/list"}>Event list</Link>
-          </li>
-          <li>
-            <Link to={"/events/ical"}>iCal</Link>
-          </li>
-        </ul>
-      </nav>
+      <Nav />
 
       <hr />
 
